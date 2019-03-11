4841320fbfe7b3a1ec997f8e6b7cbe3103b02bb3
Carlos Daniel <carlos.d.m.s@hotmail.com>
2019-03-10 19:21:46 -0600

    jwt configuration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->smallInteger('phone_number');
+            $table->timestamp('birthdate');
+            $table->string('country');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
